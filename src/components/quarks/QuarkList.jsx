335e600c7445245b645b378ed97302e79e9b1ffc
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2023-02-15 19:57:40 +0200

    Add join and create quark buttons, non functional

--- src/components/quarks/QuarkList.jsx
+++ src/components/quarks/QuarkList.jsx
@@ -1,11 +1,23 @@
 import {useContext} from "react";
 import {MainContext} from "../../contexts/MainContext";
+import {Tooltip} from "react-tooltip";
+import joinQuark from "../../assets/joinQuark.png";
+import createQuark from "../../assets/createQuark.png";
 
 export function QuarkList() {
 	let mainContext = useContext(MainContext);
 	return (
 		<div className="quarkList">
 			{mainContext.quarkBoxes}
+
+			<div className="quarkBox">
+				<img id="joinQuark" data-tooltip-content="Join a Quark" onClick={() => {}} width={"48px"} height={"48px"} src={joinQuark} alt={"Join a Quark"} className="quarkImage"></img>
+				<Tooltip className="quarkTip" anchorId="joinQuark" positionStrategy={"fixed"} place={"left"} style={{opacity: 1, backgroundColor: "var(--tooltip)"}} />
+			</div>
+			<div className="quarkBox">
+				<img id="createQuark" data-tooltip-content="Create a Quark" onClick={() => {}} width={"48px"} height={"48px"} src={createQuark} alt={"Create a Quark"} className="quarkImage"></img>
+				<Tooltip className="quarkTip" anchorId="createQuark" positionStrategy={"fixed"} place={"left"} style={{opacity: 1, backgroundColor: "var(--tooltip)"}} />
+			</div>
 		</div>
 	);
 }
